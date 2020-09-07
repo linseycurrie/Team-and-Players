3a19100f8a99895bd6a77f3a5905e8f43c0f113f
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2020-09-07 14:59:26 +0100

    second commit

--- src/team.py
+++ src/team.py
@@ -8,14 +8,16 @@ class Team:
     def add_player(self, player):
         self.players.append(player)
 
-    def change_coach(self, new_coach):
-        self.coach = new_coach
+    # def change_coach(self, new_coach):
+    #     self.coach = new_coach
 
     def has_player(self, desired_player):
         for player in self.players:
             if player == desired_player:
                 return True
         return False
+
+    # return self.players.count(desired_player) > 0
         
     def points(self):
         return self.points
@@ -23,8 +25,8 @@ class Team:
     def play_game(self, score):
         if score == True:
             self.points += 3
-        else score == False:
-            pass
+        # elif score == False:
+            # pass
 
         
 
